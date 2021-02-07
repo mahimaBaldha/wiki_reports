783e90515b1dacd665e347b59683c46e2bc06ab7
Mahima Baldha <45919370+mahimaBaldha@users.noreply.github.com>
2021-02-07 22:04:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    api_call()
+    task()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
