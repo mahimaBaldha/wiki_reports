4745c46a4ac8c8cb035b8d0dfb4f0a7203e87ffd
mahimaGlib <mahima@glib.ai>
2021-02-09 19:07:59 +0530

    correction

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-from reports import api_call
+from reports import task
 
 
 def print_hi(name):
@@ -12,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    api_call()
+    task()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- reports.py
+++ reports.py
@@ -16,7 +16,7 @@ def generate_reports(previous_minute):
             date_time_obj = datetime.datetime.strptime(response['meta']['dt'], "%Y-%m-%dT%H:%M:%SZ").strftime(
                 "%Y-%m-%d %H:%M")
             meta_data = response['meta']
-            if date_time_obj <= previous_minute.strftime("%Y-%m-%d %H:%M"):
+            if date_time_obj < previous_minute.strftime("%Y-%m-%d %H:%M"):
                 # DOMAIN REPORT
                 domain = meta_data['domain']
                 same_domain = list(filter(lambda report_data: report_data['domain'] == domain, domains_report))
@@ -30,7 +30,8 @@ def generate_reports(previous_minute):
                 if domain == "en.wikipedia.org":
                     if "performer" in response and "user_edit_count" in response:
                         wiki_user = list(
-                            filter(lambda report_data: report_data['user'] == response["performer"]["user_text"], user_report))
+                            filter(lambda report_data: report_data['user'] == response["performer"]["user_text"],
+                                   user_report))
                         if wiki_user:
                             wiki_user[0]["user_edit_count"] = response["performer"]["user_edit_count"] \
                                 if response["performer"]["user_edit_count"] > wiki_user[0]["user_edit_count"] \
@@ -44,6 +45,7 @@ def generate_reports(previous_minute):
                 break
         except ValueError:
             pass
+
     print("Total number of Wikipedia Domains Updated: {}".format(len(domains_report)))
     # for data in domains_report:
     for data in sorted(domains_report, key=lambda data: len(data['title']), reverse=True):
@@ -58,26 +60,28 @@ def task():
     current_time = datetime.datetime.now()
     # 5 hours and 30 minutes delay as api does not return current minute data. instead its time
     # is lesser than current time 5 hours and 30 minutes
-    one_minute = datetime.timedelta(hours=5, minutes=30, seconds=4)
+    one_minute = datetime.timedelta(hours=5, minutes=29, seconds=5)
     previous_minute = current_time - one_minute
     # this will ensure that only previous one minute data has to be considered
+    print("One Minute report : ")
     generate_reports(previous_minute)
     pass
 
 
 def bonus_task():
     current_time = datetime.datetime.now()
-    five_minute = datetime.timedelta(hours=5, minutes=35, seconds=4)
+    five_minute = datetime.timedelta(hours=5, minutes=25, seconds=5)
     previous_minute = current_time - five_minute
     # this will ensure that previous 5 minute data has to be considered
+    print("Last Five Minutes report : ")
     generate_reports(previous_minute)
     pass
 
 
-# report would be generated every minute
-schedule.every(1).minutes.do(task)
-schedule.every(1).minutes.do(bonus_task)
-
+# # # report would be generated every minute
+schedule.every(0).minutes.do(task)
+schedule.every(0).minutes.do(bonus_task)
+# #
 while True:
     schedule.run_pending()
     time.sleep(1)
